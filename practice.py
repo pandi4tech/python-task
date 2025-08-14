a77ef92a7c88d14ea4b7a32f3aeb69219923b027
pandidurai <pandiduraisolai@gmail.com>
2025-08-14 09:35:25 +0530

    factorial

--- practice.py
+++ practice.py
@@ -179,8 +179,11 @@
 
 
 def factorial(num):
-    if num == 0:
+    if num == 0 or num == 1:
         return 1
+    # elif num == 1:
+    #     return 1
     else:
         return num * factorial(num-1)
+
 print(factorial(6))
