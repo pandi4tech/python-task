0a267908011dce494a4732a8d8e8c04cf51801cd
pandidurai <pandiduraisolai@gmail.com>
2025-08-14 09:25:25 +0530

    factorial

--- practice.py
+++ practice.py
@@ -176,3 +176,11 @@
 # n="pandi"
 # reversede=n[::-1]
 # print(reversede)
+
+
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num-1)
+print(factorial(6))
